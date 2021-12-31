12abe9df5fac78322095dbc5bd9c013c3922b2de
samhuang1992 <bob60117@gmail.com>
2021-12-31 13:58:09 +0800

    feat: add mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/URL_Shorten')
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+// 連線錯誤
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+module.exports = db
